04852a5628c98b59c1def96245450c06309e617a
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2024-12-06 19:48:04 +0200

    fix dialog

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { effects, store } from './store/store';
@@ -10,8 +10,10 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
     provideRouter(routes),
     provideHttpClient(),
     provideStore(store),
-    provideEffects(effects)]
+    provideEffects(effects),
+  ],
 };
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,42 +1,46 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogContent, MatDialogTitle, MAT_DIALOG_DATA, MatDialog,MatDialogActions,
-  MatDialogClose, 
-  MatDialogModule} from '@angular/material/dialog';
+import {
+  MatDialogContent,
+  MatDialogTitle,
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogModule,
+} from '@angular/material/dialog';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent, IconSpriteModule, MatButtonModule, MatDialogActions,
-    MatDialogClose, MatDialogModule],
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    IconSpriteModule,
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogModule,
+  ],
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.css'
+  styleUrl: './modal.component.css',
 })
 export class ModalComponent implements OnInit {
   public data = inject(MAT_DIALOG_DATA);
-  public address!: string[]
-  public conditions!: string[]
-  public age!: string[]
-  public fixMileage!: string
-  public price!: number
-
-  constructor( private dialog: MatDialog){}
+  public address!: string[];
+  public conditions!: string[];
+  public age!: string[];
+  public fixMileage!: string;
+  public price!: number;
 
   ngOnInit(): void {
-    console.log(this.data.card)
-    this.address = this.data.card.address.split(', ')
-   this.conditions = this.data.card.rentalConditions.split("\n");
-	 this.age = this.conditions[0].split(": ");
-   this.conditions.shift()
-   this.fixMileage = this.data.card.mileage.toLocaleString();
-   this.price = this.data.card.rentalPrice.replace(/[^0-9]/g, "")
+    console.log(this.data.card);
+    this.address = this.data.card.address.split(', ');
+    this.conditions = this.data.card.rentalConditions.split('\n');
+    this.age = this.conditions[0].split(': ');
+    this.conditions.shift();
+    this.fixMileage = this.data.card.mileage.toLocaleString();
+    this.price = this.data.card.rentalPrice.replace(/[^0-9]/g, '');
   }
-
-  // public closeModal(){
-  //   this.dialog.closeAll()
-  // }
-  // handleClick(event: Event) {
-  //   event.stopPropagation();
-  // }
 }
