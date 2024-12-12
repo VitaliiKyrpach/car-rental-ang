a586b26c2ef2dd53db9be5a8caceee3548b04159
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-12-12 11:03:15 +0200

    add homePage gif and start filters

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @Component({
   selector: 'car-filters',
   standalone: true,
-  imports: [],
+  imports: [MatDividerModule, ReactiveFormsModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
 export class FiltersComponent {
-
+  public filterGroup = new FormGroup({
+    brand: new FormControl(''),
+    price: new FormControl(''),
+    from: new FormControl(''),
+    to: new FormControl('')
+  })
 }
